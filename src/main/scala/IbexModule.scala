ceecfb73df305251b18d88c5e3e3df64425e3be5
shahzaibk23 <shahzaibceo@gmail.com>
2022-09-29 21:35:02 +0500

    IBEX CHISEL module added

---
+++ src/main/scala/IbexModule.scala
@@ -0,0 +1,20 @@
+package ibex_jigsaw
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental._
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+
+class IbexModule extends Module{
+  val io = IO(new IbexBundle)
+    val coreParams = IbexCoreParams()
+    val core = Module(new BlackBoxIbexCore(coreParams))
+    core.io <> io
+}
+
+
+
+object main extends App {
+    (new ChiselStage).emitSystemVerilog(new IbexModule())
+
+}
